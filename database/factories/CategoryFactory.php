14f1511d48faa5c0261360ccbd8d9d70cdc08aec
Ucef Dev <contact.ucefdev@gmail.com>
2021-11-28 01:11:54 +0100

    add database factories

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+
+        $name = $this->faker->jobTitle();
+        $nameArr = explode(" ",$name);
+        $name = trim($nameArr[0]);
+
+
+        return [
+            'name' => $name,
+            'slug' =>  Str::slug($name),
+        ];
+    }
+}
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
           //  $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('adress');
+            $table->string('address');
             $table->string('city');
             $table->string('state');
             $table->string('zip_code');
