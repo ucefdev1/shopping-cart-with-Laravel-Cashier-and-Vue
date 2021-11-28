1535c3798d4a866aaaa1792841917414db1e72e1
Ucef Dev <contact.ucefdev@gmail.com>
2021-11-28 14:56:25 +0100

    fix factories

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+use Illuminate\Support\Arr;
 
 class ProductFactory extends Factory
 {
@@ -13,8 +15,14 @@ class ProductFactory extends Factory
      */
     public function definition()
     {
+        $productSuffixes= ['Shoes','Sweater','Pants'];
+        $name = $this->faker->company().' '.Arr::random($productSuffixes);
         return [
-            //
+            'name'=>$name,
+            'slug'=>Str::slug($name),
+            'description'=>$this->faker->realText(320),
+            'price'=>$this->faker->numberBetween(10000,100000), // 100$ - 1000$
+
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use  \App\Models\Product;
+use  \App\Models\Category;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+       Product::factory(20)->create();
+       Category::factory(5)->create();
     }
 }
