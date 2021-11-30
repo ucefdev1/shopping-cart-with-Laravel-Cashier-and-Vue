98e4acd7726d8a179d4491cf356b2b3a7c1fd158
Ucef Dev <contact.ucefdev@gmail.com>
2021-11-30 01:39:01 +0100

    Building the vue Frontend

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 Vue.use(Vuex)
 import VueRouter from 'vue-router'
+import axios from 'axios';
 Vue.use(VueRouter)
 
 
@@ -13,3 +14,64 @@ const router = new VueRouter({
     mode: 'history',
     routes: require('./routes.js')
 })
+
+
+const store =  new Vuex.Store({
+    
+    
+    state: {
+        products:  [],
+        cart: [],
+        order: {}
+    },
+
+    mutations: {
+        updateProducts(state, products){
+            state.products = products;
+        },
+        addToCart(state, product){
+            let productInCartInbox = state.cart.findIndex(item => item.slug === product.slug);
+            if(productInCartInbox !== -1){
+                state.cart[productInCartInbox].quantity++; 
+                return;
+            }
+
+            product.quantity = 1;
+            state.cart.push(product);
+        },
+        removeFromCart(state, index){
+            state.cart.splice(index, 1);
+        },
+        updateOrder(state,order){
+            state.order =order;
+        },
+        updateCart(state,cart){
+            state.cart =cart;
+        }
+    },
+    actions:{
+        getProducts({ commit }){
+            axios.get('/api/products').then((response) => {
+                commit('updateProducts',response.data)
+            })
+            .catch((error) => console.error(error));
+        },
+        clearCart({commit}){
+            commit('updateCart', []);
+        }
+    }
+
+}
+);
+
+
+const app = new Vue({
+    router,
+    store,
+    el: '#app',
+    created(){
+        store.dispatch('getProducts')
+        .then(_ => {})
+        .catch((error) => console.error(error));
+    }
+});
---
+++ resources/js/routes/Products/Index.vue
@@ -0,0 +1,17 @@
+<template>
+    <section class="text-gray-700 body-font">
+        <div class="container px-5 py624 mx-auto">
+            <div class="flex flex-wrap -m-4">
+                knskn
+            </div>
+        </div>
+    </section>
+</template>
+
+
+<script>
+
+export default {
+    
+}
+</script>
